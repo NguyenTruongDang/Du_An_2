4273d661dca41159fec6a7c449c47058cf0d75d4
nguyentruongdang <nguyentruongdang1010@gmail.com>
2019-11-29 19:01:41 +0700

    Up load freekok By Đăng

--- public/template/js/style.js
+++ public/template/js/style.js
@@ -1,3 +1,28 @@
+// // Ẩn hiện đăng ký đăng nhập
+// $(document).ready(function () {
+//   $('.signup-container').hide();
+//   $('.login-container').hide();
+//   $('#dangnhap').click(function () {
+//       $('.login-container').slideToggle();
+//       $('.signup-container').fadeOut();
+//       $('.login-container').css({"position": "absolute", "z-index": "99999","margin": "7%" });
+//       $('.login-container div.row').css("background-color", "#fff");
+//   });
+//   $('#dangky').click(function () {
+//       $('.signup-container').slideToggle();
+//       $('.login-container').fadeOut();
+//       $('.signup-container').css({"position": "absolute", "z-index": "99999","margin": "7%" });
+//       $('.signup-container div.row').css("background-color", "#fff");
+//   });
+//   $('#chuyendk').click(function (event) {
+//       $('.signup-container').fadeIn();
+//       $('.login-container').fadeOut();
+//   });
+//   $('#chuyendn').click(function (event) {
+//       $('.signup-container').fadeOut();
+//       $('.login-container').fadeIn();
+//   });
+// });
 
 // Ẩn hiện about, timeline
 $(document).ready(function () {
